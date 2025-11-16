517f0c6562ba8dcf4b59d7808f8c07d3917c8b13
sakibjpz <jpzsakib@gmail.com>
2025-11-17 00:25:15 +0600

    started frontend development

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,16 @@ use App\Http\Controllers\backend\InstructorProfileController;
 use App\Http\Controllers\backend\AdminProfileController;
 use App\Http\Controllers\backend\CategoryController;
 use App\Http\Controllers\backend\SubcategoryController;
+use App\Http\Controllers\frontend\FrontendDashboardController;
+use App\Http\Controllers\backend\SliderController;
+use App\Http\Controllers\frontend\CartController;
 
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -68,12 +71,29 @@ Route::middleware(['auth', 'verified', 'role:instructor'])
     
 
 
-// Normal user profile routes
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// // Normal user profile routes
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+  /* Control Slider */
+    Route::resource('slider', SliderController::class);
+
+//Frontend Route
+
+Route::get('/', [FrontendDashboardController::class, 'home'])->name('frontend.home');
+
+
+
+
+/* Cart Controller */
+Route::get('/cart', [CartController::class, 'index'])->name('cart');
+Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.add');
+Route::get('/cart/all', [CartController::class, 'cartAll']);
+Route::get('/fetch/cart', [CartController::class, 'fetchCart']);
+Route::post('/remove/cart', [CartController::class, 'removeCart']);
 
 // Authentication routes (Breeze/Fortify/etc.)
 require __DIR__.'/auth.php';
